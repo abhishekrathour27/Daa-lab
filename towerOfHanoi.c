08c80d0ecf2488a80bcb232a073abf063fd14dab
Abhishek singh <abhishekrathour96081@gmail.com>
2025-07-21 07:08:08 +0530

    recode

--- towerOfHanoi.c
+++ towerOfHanoi.c
@@ -1,33 +1,20 @@
-#include <stdio.h>
+#include<stdio.h>
 
-// Recursive function to solve Tower of Hanoi
-void towerOfHanoi(int n, char source, char auxiliary, char destination) {
-    if (n == 1) {
-        // Base case: only one disk, move directly
-        printf("Move disk 1 from %c to %c\n", source, destination);
-        return;
-    }
-
-    // Step 1: Move n-1 disks from source to auxiliary
-    towerOfHanoi(n - 1, source, destination, auxiliary);
-
-    // Step 2: Move the nth (largest) disk from source to destination
-    printf("Move disk %d from %c to %c\n", n, source, destination);
-
-    // Step 3: Move the n-1 disks from auxiliary to destination
-    towerOfHanoi(n - 1, auxiliary, source, destination);
+void towerofhenoi(int n,char A,char B, char C){
+    if(n==1){
+        printf("Move the disk from %c to %c\n",A,B);
+        return ;
+    }   
+    else{
+        towerofhenoi(n-1,A,C,B); 
+        printf("Move the disk from %c to %c\n",A,B);
+        towerofhenoi(n-1,C,B,A);
+    }  
 }
-
-int main() {
+int main(){
     int n;
-
-    // Input number of disks
-    printf("Enter the number of disks: ");
-    scanf("%d", &n);
-
-    // Call the recursive function
-    printf("\nSteps to solve Tower of Hanoi for %d disks:\n", n);
-    towerOfHanoi(n, 'A', 'B', 'C'); // A = source, B = auxiliary, C = destination
-
+    printf("Enter the number of disk\n");
+    scanf("%d",&n); 
+    towerofhenoi(n,'A','B','C');
     return 0;
 }
